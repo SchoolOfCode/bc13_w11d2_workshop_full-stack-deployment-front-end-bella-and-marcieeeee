5d8357db08caf9570212e243bfc63e4e1ca33268
PeterM-13 <petershotbox@gmail.com>
2022-12-07 10:04:48 +0000

    added hover effects

--- src/components/CompletedFilter/index.js
+++ src/components/CompletedFilter/index.js
@@ -5,7 +5,7 @@ export default function CompletedFilter({handleChange}){
     return (
 <select onChange={handleChange}>
     <option value='all'>All</option>
-    <option value='true' >Completed</option>
+    <option value='true' >Complete</option>
     <option value='false'>Uncomplete</option>
 </select>
     )};
